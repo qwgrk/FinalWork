156b97ce64a554b4d44580d5bbb71df950836112
Egor <Egor@mail.com>
2026-04-05 19:49:51 +0300

    Add proga file

---
+++ Proga10.cpp
@@ -0,0 +1,34 @@
+#include <iostream>
+#include <fstream>
+
+using namespace std;
+
+int main() {
+    int a;
+    int b;
+    int c;
+
+    cout << "Введите 3 числа: ";
+    cin >> a >> b >> c;
+
+    ofstream file("numbers.txt"); // открытие файла для записи
+
+    if (file.is_open()) {
+        file << "a = " << a << endl;
+        file << "b = " << b << endl;
+        file << "c = " << c << endl;
+
+        if (a > 0) { // проверка первой переменной
+            file << "Первая переменная положительная" << endl;
+        } else {
+            file << "Первая переменная не положительная" << endl;
+        }
+
+        file.close();
+        cout << "Данные записаны в файл numbers.txt" << endl;
+    } else {
+        cout << "Ошибка открытия файла!" << endl;
+    }
+
+    return 0;
+}
